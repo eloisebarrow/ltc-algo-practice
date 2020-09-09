b2b05465914101ce7e5f711b257b45c2531d8fe0
Eloise Barrow <eloise@chartbeat.com>
2020-09-09 09:51:04 -0400

    add day 1 js solution

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-// TUESDAY, SEPTEMBER 8, 2020
+// DAY 1
 
 // Given a two-digit integer, return the sum of its digits.
 const addDigits = (n) => {
-
+  n = n.toString().split('')
+  return parseInt(n[0]) + parseInt(n[1])
 }
 
 console.log(addDigits(29)) // 11
