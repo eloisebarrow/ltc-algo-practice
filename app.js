fe89a303aa301a61aa6ebdbfb47989783c587fbe
Eloise Barrow <eloise@chartbeat.com>
2020-09-11 09:37:03 -0400

    add oddsEvens js solution

--- app.js
+++ app.js
@@ -8,3 +8,39 @@ const addDigits = (n) => {
 
 console.log(addDigits(29)) // 11
 console.log(addDigits(30)) // 3 
+
+// PROBLEM 2
+
+// Write a function called oddsEvens that given a string, prints its even-indexed and odd-indexed characters as space-separated strings on a single line.
+
+const oddsEvens = (input) => {
+  let left = '';
+  let right = '';
+  for (let i = 0; i < input.length; i += 1) {
+    if (i % 2 === 0) {
+      left += input[i];
+    } else {
+      right += input[i];
+    }
+  }
+  return `${left} ${right}`;
+}
+
+console.log(oddsEvens('Hacker')) // Hce akr
+
+// PROBLEM 3
+
+const isAnagram = () => {
+
+}
+
+console.log(isAnagram("cinema", "iceman")) // true
+console.log(isAnagram("randum", "werdz")) // false
+
+// PROBLEM 4
+
+const longestStrings = () => {
+
+}
+
+console.log(longestStrings(["aba", "aa", "ad", "vcd", "aba"])) // ["aba", "vcd", "aba"]
