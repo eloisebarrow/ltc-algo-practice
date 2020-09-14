ee39d3251ac5ca0c58536773421c87fbd9d5e12d
Eloise Barrow <eloise@chartbeat.com>
2020-09-14 09:23:50 -0400

    add problem 4 js solution

--- javascript/problem-004.js
+++ javascript/problem-004.js
@@ -1,7 +1,18 @@
 // PROBLEM 4
 
-const longestStrings = () => {
-  
+const longestStrings = (strs) => {
+  let output = []
+  let longest = 0
+  for (let i = 0; i < strs.length; i++) {
+    let current = strs[i]
+    if (current.length > longest) {
+      longest = current.length;
+      output = [current]
+    } else if (current.length === longest) {
+      output.push(current)
+    }
+  }
+  return output;
 }
 
 console.log(longestStrings(["aba", "aa", "ad", "vcd", "aba"])) // ["aba", "vcd", "aba"]
