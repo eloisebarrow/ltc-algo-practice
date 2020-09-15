23191a56b5a4bfb4a32e61efefc7dcdab3e1475b
Eloise Barrow <eloise@chartbeat.com>
2020-09-15 09:23:20 -0400

    add problem 5 solution

--- javascript/problem-005.js
+++ javascript/problem-005.js
@@ -1,7 +1,11 @@
 // PROBLEM 5
 
-const largestNumber = () => {
-
+const largestNumber = (arrNums) => {
+  let largest = 0;
+  for (let i = 0; i < arrNums.length; i++) {
+    arrNums[i] > largest ? largest = arrNums[i] : null
+  }
+  return largest
 }
 
 console.log(largestNumber([1,2,5,10])) // 10
