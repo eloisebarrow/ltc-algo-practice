964833ff480c7cca85806a32e423fd6690907f89
Eloise Barrow <eloise@chartbeat.com>
2020-09-17 09:17:09 -0400

    add problem 7 solution, not including punctuation

--- javascript/problem-007.js
+++ javascript/problem-007.js
@@ -1,8 +1,17 @@
-const isPalindrome = (input) => {
+const reversed = (s) => {
+  let output = ''
+  for (let i = s.length - 1; i >= 0; i--) {
+    output += s[i]
+  }
+  return output
+}
 
+const isPalindrome = (input) => {
+  input = input.toLowerCase().split(' ').join('')
+  return input === reversed(input)
 }
 
 console.log(isPalindrome("tacocat")) // true
 console.log(isPalindrome("nottacocat")) // false
 console.log(isPalindrome("Race car")) // true
-console.log(isPalindrome("a man, a plan, a canal - panama")) // true
+// console.log(isPalindrome("a man, a plan, a canal - panama")) // true
