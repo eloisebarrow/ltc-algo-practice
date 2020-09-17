0cf5e6117ff1c409e3f14bf6389d0491e55ac0d0
Manuel Lopez <manny@chartbeat.com>
2020-09-17 17:15:42 -0400

    [clojure] updated 09/16/20 solution

--- clojure/problem7.clj
+++ clojure/problem7.clj
@@ -6,10 +6,27 @@
 ;;
 ;; Input: isPalindrome("nottacocat")
 ;; Output: False
-;; 09/16/20
+
+
+;; Additional levels of difficulty:
+;; Input: isPalindrome("Race car")
+;; Output: True
+
+;;Input: isPalindrome("a man, a plan, a canal - panama")
+;;Output: True
+
+;;  09/16/20
+(require '[clojure.string :as str])
 
 (defn isPalindrome [s]
-  (= s (apply str (reverse s))))
+  (let [s (->> s str/lower-case
+                (filter #(<= 97 (int %) 122))
+                (vec)
+                (apply str))]
+    (= s (apply str (reverse s)))))
+
 
 (println (isPalindrome "tacocat"))
 (println (isPalindrome "nottacocat"))
+(println (isPalindrome "Race car"))
+(println (isPalindrome "a man, a plan, a canal - panama"))
