799d2b1f868d67089bfe3f70451d7f661a61cf27
Eloise Barrow <eloise@chartbeat.com>
2020-09-30 08:39:54 -0400

    add problem 13 js boilerplate solution

---
+++ javascript/problem-013.js
@@ -0,0 +1,15 @@
+// Write a function called isPrime, that takes in a positive integer and returns
+// True if number is Prime, otherwise false
+
+// Input: isPrime(10)
+// Output: False
+
+// Input: isPrime(13)
+// Output: True
+
+const isPrime = (n) => {
+
+}
+
+console.log(isPrime(10)) // false
+console.log(isPrime(13)) // true
