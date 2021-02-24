149d1ec060a72d37b7c97db5c24168359b806426
Eloise Barrow <eloise@chartbeat.com>
2021-02-24 10:04:26 -0500

    partial js problem 23 solution, add problem 24

--- README.md
+++ README.md
@@ -332,3 +332,22 @@ Subarrays with zero-sum are
 { 3, 1, 3, 1, -4, -2, -2 }
 { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
 ```
+
+### Problem 24 (Arrays) - 0-1 Knapsack problem https://www.techiedelight.com/0-1-knapsack-problem/
+
+In the 0-1 Knapsack problem, we are given a set of items, each with a weight and a value, and we need to determine the number of each item to include in a collection so that the total weight is less than or equal to a given limit and the total value is as large as possible.
+
+Note that the items are indivisible; we can either take an item or not (0-1 property).
+
+```
+Input:
+ 
+value = [ 20, 5, 10, 40, 15, 25 ]
+weight = [ 1, 2, 3, 8, 7, 4 ]
+int W = 10
+ 
+Output: Knapsack value is 60
+ 
+value = 20 + 40 = 60
+weight = 1 + 8 = 9 < W
+```
--- javascript/problem-023.js
+++ javascript/problem-023.js
@@ -1,3 +1,5 @@
+/********************  UNFINISHED *************/
+
 // Given an integer array, print all subarrays with zero-sum
 
 // Input:  { 4, 2, -3, -1, 0, 4 }
@@ -18,3 +20,24 @@
 // { 3, 1, -4 }
 // { 3, 1, 3, 1, -4, -2, -2 }
 // { 3, 4, -7, 3, 1, 3, 1, -4, -2, -2 }
+
+const printSubarrays = (arr) => {
+  // let subarrays = [];
+  for (let i = 0; i <= arr.length; i++) {
+    let sum = 0;
+    let current = arr[i];
+    // let currentArray = [current];
+    sum += current;
+    for (let j = i + 1; j <= arr.length - 1; j++) {
+      // currentArray.push(arr[j]);
+      sum += arr[j];
+      if (sum === 0) subarrays.push(currentArray);
+      // sum += arr[j];
+    }
+  }
+  return subarrays.forEach(arr => {
+    console.log(arr)
+  })
+}
+
+printSubarrays([3, 4, -7, 3, 1, 3, 1, -4, -2, -2])
